0a249134837dbd7f7c45e2a50218764ed8a70b75
Nathan T. Baker <nathantbaker@gmail.com>
2017-02-08 15:01:49 -0600

    adding customer, customerregistrar and related test

---
+++ app/customer.py
@@ -0,0 +1,18 @@
+class Customer(object):
+    """ The Customoer class creates a new Customer with data passed to it.
+
+    Method List   __init__ instansiates the new Customer with customer_name,
+                  city, state, postal_code, and phone_number
+    Arguments     The object argument lets the Customer class inherit properites of object
+    Author        Nate Baker, Python Ponies
+    """
+    def __init__(self, customer_name, city, state, postal_code, phone_number):
+        """
+        A new customer is created based on the arguments passed in: customer_name, city, state, postal_code, and phone_number. The active switch is set to false by default and turned on when an active user is selected.
+        """
+        self.customer_name = customer_name
+        self.city = city
+        self.state = state
+        self.postal_code = postal_code
+        self.phone_number = phone_number
+        self.active = False
---
+++ tests/TestRegisterCustomer.py
@@ -0,0 +1,45 @@
+import unittest
+import sys
+sys.path.append('../')
+from app.customer import *
+from app.customerregistrar import *
+# Registrar and Customer are classes of registercustomer
+
+class TestRegisterCustomer(unittest.TestCase):
+    """
+    This class tests everything related to registering a user.
+
+    Method List   test_customer_can_register
+    Arguments     unittest.TestCase allows the unittest model to know what to test.
+    Author        Nate Baker, Python Ponies
+    """
+
+    def test_customer_can_register(self):
+        """ This method tests if a customer can successfully register. A customer should be able to register by entering the following information:
+
+            - customer name
+            - city
+            - state
+            - postal code
+            - phone number
+        """
+
+        nate = Customer("nate",      # customer name
+                        "nashville", # city
+                        "tn",        # state
+                        "12345",     # postal code
+                        "1234567")   # phone number
+
+        # Test that nate is an instance of the Customer class
+        self.assertIsInstance(nate, Customer)
+
+        # Register the customer nate
+        CustomerRegistrar.register(nate)
+
+        # Test that it's true that the customer is registered
+        self.assertTrue(CustomerRegistrar.check_if_registered(nate))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
