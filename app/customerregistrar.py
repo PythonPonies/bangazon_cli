accdf9a4871ae69ce1f134f5c48da900cc661079
Nathan T. Baker <nathantbaker@gmail.com>
2017-02-09 14:26:47 -0600

    remove street address from sql statements

--- app/customerregistrar.py
+++ app/customerregistrar.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class CustomerRegistrar():
     """ CustomerRegistrar handles registering a customer.
 
@@ -6,9 +8,31 @@ class CustomerRegistrar():
     """
 
     def register(customer):
+        print("customer method runs")
         """ The registrar method takes a customer as an argument and pushed customer data up to the database.
         """
-        data_to_push_to_database = customer
+
+        # Connect to the database
+        connection = sqlite3.connect('../bangazon.db')
+
+        try:
+            with connection.cursor() as cursor:
+                # Create a new record
+                sql = "INSERT INTO `Customers` VALUES (null, '{}', '{}', '{}', '{}', '{}').format('nate', )"
+                cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            connection.commit()
+
+            with connection.cursor() as cursor:
+                # Read a single record
+                sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+                cursor.execute(sql, ('webmaster@python.org',))
+                result = cursor.fetchone()
+                print(result)
+        finally:
+            connection.close()
 
     def check_if_registered(customer):
         return True
