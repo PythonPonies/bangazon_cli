3601ff944f4c91fed6284d905c1fc376cf04646e
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2017-02-09 13:23:17 -0600

    fixed file names

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,7 +1,7 @@
 from test_active_user import *
-from TestProductOnOrder import *
-from TestRegisterCustomer import *
-from TestUserPaymentType import *
+from test_product_on_order import *
+from test_register_customer import *
+from test_user_payment_type import *
 from test_customer_can_tie_payment_type_to_order import *
 
 # This file imports all tests into one file so all tests can easily be checked by running `python runtests.py`
