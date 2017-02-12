9d96cbb7ce26721591ce67634fad40ba7f45852f
Ikechi <consumerike@gmail.com>
2017-02-11 19:46:44 -0600

    passing test for check_cart_contains_items

--- app/orderfinalizer.py
+++ app/orderfinalizer.py
@@ -1,58 +1,81 @@
+import sqlite3
+
+
+
 class OrderFinalizer():
-	"""
-	purpose: The OrderFinalizer class contains  methods and properties related
-	to the ordering of products from creation to submission.
 
-	Methods: 
-		- check_cart_contains_items
-		- complete_order
-		-check_order_is_complete
-	"""
+    """
+    purpose: The OrderFinalizer class contains  methods and properties related
+    to the ordering of products from creation to submission.
 
-	def check_cart_contains_items(self, order):
-		"""
-		purpose: Determine whether the cart has items by returning status
-		Author: Ike
-			variables: 
-				- status: A flag set to False that can toggle to True
-				- order: A list of products, an empty list returns False
+    Methods: 
+        - check_cart_contains_items
+        - complete_order
+        -check_order_is_complete
+    """
 
-		"""
-		status = False
-		self.order = order
-		if order:
-			status = True
-		return status
+    def check_cart_contains_items(self):
 
-	def complete_order(self, order, payment_type):
-		"""
-		purpose: Complete an order by tying a payment_type to an order
-		author: Ike
-		parameters:
-			- order: list of products
-			-payment_type: data type containing payment_type
-		
-		variables:
-			- status: flag to ensure the order contains items (is active?)
-		"""
-		status = True
-		self.order = order
-		self.payment_type =  payment_type
-		if status:
-			 order["payment_type"] = self.payment_type
+        """
+        purpose: Determine whether the cart has items by returning status
+        Author: Ike
+            variables: 
+                - status: A flag set to False that can toggle to True
+                - order: A list of products, an empty list returns False
+
+        """ 
+        with sqlite3.connect("../bangazon.db") as databae:
+            cursor = databae.cursor()
+            try:
+                cursor.execute('SELECT customerId FROM `Customers` WHERE active = 1'
+                )
+                cid = cursor.fetchone()
+                print(cid, "this is currentid")
+                cursor.execute("""
+                    SELECT * FROM Orders
+                    WHERE payment_complete = 0
+                    AND customerId = '{}'
+                    """
+                .format(cid))
+                selected_order = cursor.fetchall() #this is the one order from active user
+                cursor.execute("SELECT productId FROM ProductsOnOrders WHERE orderId = '{}'")#.format(selected_order[0])
+                products_on_selected_order = cursor.fetchall()
+                print(products_on_selected_order, "products on selected order")
+                return products_on_selected_order
+
+            except sqlite3.OperationalError:
+                return False
 
 
-	def check_order_is_complete(self,order, payment_type):
-		"""
-		purpose: Check that an order is complete by returning a True indicator
-		author: Ike
-		parameters:
-			-order
-			-payment_type
-		"""
-		order = {"userId":"42", "products":['peanut butter', 'jelly', 'bread'], "payment_type": "10"}
-		self.payment_type = order["payment_type"]
-		status = True
-		order_status = True
-		return order_status
+    def complete_order(self, order, payment_type):
+        """
+        purpose: Complete an order by tying a payment_type to an order
+        author: Ike
+        parameters:
+            - order: list of products
+            -payment_type: data type containing payment_type
+        
+        variables:
+            - status: flag to ensure the order contains items (is active?)
+        """
+        status = True
+        self.order = order
+        self.payment_type =  payment_type
+        if status:
+             order["payment_type"] = self.payment_type
 
+
+    def check_order_is_complete(self,order, payment_type):
+        """
+        purpose: Check that an order is complete by returning a True indicator
+        author: Ike
+        parameters:
+            -order
+            -payment_type
+        """
+
+        with sqlite3.connect("../bangazon.db") as databae:
+            cursor = databae.cursor()
+            cursor.execute('SELECT customerId FROM `Customers` WHERE active = 1')
+            cid = cursor.fetchall()
+            print(cid)
--- tests/test_customer_can_tie_payment_type_to_order.py
+++ tests/test_customer_can_tie_payment_type_to_order.py
@@ -3,9 +3,12 @@ import sys
 sys.path.append('../')
 from app.ordermanager import *
 from app.orderfinalizer import *
+from app.productonordermanager import *
 from app.customer import *
 from app.paymentmanager import *
 from app.order import *
+from app.customerregistrar import *
+from app.customerstatusmanager import *
 
 
 
@@ -18,13 +21,16 @@ class TestCompleteOrder(unittest.TestCase):
 		methods: test_products_have_been_added
 	"""
 
-	@classmethod
-	def setUpClass(self):
-	    self.bobby = Customer("Bobby Kennedy", '343 paper street', "Boston", "MA", "98021", "206-988-8766")
-	    self.paymentmanager = PaymentManager()
-	    self.order = Order(self.bobby.customer_name)
-	    self.orderfinalizer = OrderFinalizer()
-	    self.ordermanager = OrderManager()
+	# @classmethod
+	# def setUpClass(self):
+	#     self.bobby = Customer("Bobby Kennedy", '343 paper street', "Boston", "MA", 98021, '2069888766')
+	#     self.paymentmanager = PaymentManager()
+	#     self.order = Order(self.bobby)
+	#     self.orderfinalizer = OrderFinalizer()
+	#     self.ordermanager = OrderManager()
+	#     self.productonordermanager = ProductOnOrderManager()
+	#     self.customerregistrar = CustomerRegistrar()
+
 
 	def test_check_that_an_order_is_empty(self):
 		"""
@@ -32,23 +38,51 @@ class TestCompleteOrder(unittest.TestCase):
 		author: Ike
 		methods: get_ordered_products: returns a list of products
 		"""
-		product = OrderManager()
-		self.assertEqual(product.get_products_on_order(), [])
+		jfk = Customer("John Kennedy", '1819 Heron Pointe Dr', "Nashville", "TN", 37214, '8889878888')
+		# registration = CustomerRegistrar()
+		# registration.register(jfk)
+		# # self.customerregistrar.register(robert)
+		# manage_status = CustomerStatusManager()
+		# make_jfk_active_user = manage_status.change_status(jfk.customer_name)
 
-	def test_check_that_order_can_be_completed(self):
-		"""
-		purpose: test to check if the cart is empty
-		author: Ike
-		methods: get_ordered_products: returns a list of products
-		"""
-		product = OrderManager()
-		product.products=['apples', 'oranges', 'ben and jerrys']
-		self.order.payment_type.append(self.paymentmanager.add_payment_type(self.bobby, "VISA", "123467"))
+		start_jfk_order = Order(jfk)
+		manage_order = OrderManager()
+		jfk_order = manage_order.create_order(start_jfk_order) #an order is created in the database
+		manage_products_on_order = ProductOnOrderManager()
+		current_order = manage_products_on_order.get_all_products_on_order() #get all products on active user's order
+		print(current_order, "this is current order")
+		finalize_order = OrderFinalizer()
+		active_order = finalize_order.check_cart_contains_items()
+		print(active_order, "this is active order")
+		self.assertEqual(current_order, [])
+		self.assertEqual(active_order, [])
 
-		self.assertNotEqual(product.get_products_on_order(), [])
 
-		self.assertIn(self.paymentmanager.add_payment_type(self.bobby, "VISA", "123467"), self.order.payment_type)
+		
+		# self.customerstatusmanager.change_status(self.bobby.get_customer_name())
+		# create_order = self.ordermanager.create_order(self.order)
+		# order = self.productonordermanager.get_all_products_on_order()
+		# product = OrderManager()
+		# print(order)
+		# self.assertIsNone(order)
 
+	# def test_check_that_order_can_be_completed(self):
+	# 	"""
+	# 	purpose: test to check if the cart is empty
+	# 	author: Ike
+	# 	methods: get_ordered_products: returns a list of products
+	# 	"""
+	# 	product = OrderManager()
+	# 	product.products = ['apples', 'oranges', 'ben and jerrys']
+	# 	self.order.payment_type.append(self.paymentmanager.add_payment_type(self.bobby, "VISA", "123467"))
+	# 	self.assertIsNotNone(order)
+	# 	#asserting a payment type is the value for an order.
+	# 	self.assertIn(self.paymentmanager.add_payment_type(self.bobby, "VISA", "123467"), self.order.payment_type)
+	# 	self.assertEquals(status, 1)
+
+	# 	order_status = ""
+	# 	status = CustomerStatusManager.change_status(self, Customer1.customer_name)
+	# 	self.assertEqual(order_status, 1)
 
 if __name__ == "__main__":
     unittest.main()
