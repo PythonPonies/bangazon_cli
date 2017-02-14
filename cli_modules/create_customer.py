187edfc83d738affcc30ad9ca217789dbaef03f7
Nathan T. Baker <nathantbaker@gmail.com>
2017-02-14 11:57:09 -0600

    collecting inputs for creating customer

--- cli_modules/create_customer.py
+++ cli_modules/create_customer.py
@@ -1,4 +1,22 @@
-class CLICreateCustomer():
+from app.customer import *
+from app.customerregistrar import *
+
+class CLICreateCustomer(object):
 
     def create_customer():
-        print("you typed 1")
+
+        # collect user inputs
+        customer_name  = input(" Enter customer name > ")
+        street_address = input("Enter street address > ")
+        city           = input("          Enter city > ")
+        state          = input("         Enter state > ")
+        postal_code    = input("   Enter postal code > ")
+        phone_number   = input("  Enter phone number > ")
+
+        new_customer = Customer(customer_name, street_address, city, state, postal_code, phone_number)
+
+        CustomerRegistrar.register(new_customer)
+
+        print("The customer " + customer_name + " registered successfully")
+
+        #show correct menu
