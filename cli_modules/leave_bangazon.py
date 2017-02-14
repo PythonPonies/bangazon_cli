7d1c5b22d3f9a0e32daa3cf55a06295b2a97c60f
LaDonna <boxgirl85@gmail.com>
2017-02-14 14:52:42 -0600

    created CLI for leaving app

--- cli_modules/leave_bangazon.py
+++ cli_modules/leave_bangazon.py
@@ -1,4 +1,5 @@
 class CLILeaveBangazon():
 
     def leave_bangazon():
-        print("you typed 7")
+        print(""" You have been logged out. Thank you for shopping with Bangazon. If you have an incomplete order, please note that you will either need to cancel the order or complete it. Any items added to your cart in a future session will be added to your existing incomplete order.
+        	""")
