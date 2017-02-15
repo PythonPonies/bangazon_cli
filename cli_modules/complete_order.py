819c8dda51c7ed287ec677e3eb244cd06f6dc53b
Ikechi <consumerike@gmail.com>
2017-02-15 08:43:47 -0600

    added complete order mod to commit

--- cli_modules/complete_order.py
+++ cli_modules/complete_order.py
@@ -11,26 +11,34 @@ class CLICompleteOrder():
             checkout_total = OrderFinalizer.order_total()
             print('Your total order is ' + str(checkout_total) +". ")
             submit_response = input('Ready to Purchase? (y/n)')
-            if submit_response == "Y" or "y" or "yes" or "YES":
+
+            # import pdb; pdb.set_trace()
+            signal = False
+
+            if signal == False and submit_response == "Y" or submit_response == "y" or submit_response == "yes" or  submit_response == "YES":
+                signal = True
+                print(signal, "what's signal?")
                 list_of_payment_types = PaymentManager.get_payment_types('bangazon.db')
                 for payment_type in list_of_payment_types:
                     print(payment_type[0],payment_type[1])
-            
-            selected_payment_option = input('Please choose a payment option. or Type "quit" to re-enter')
-            print(selected_payment_option, "this is the selected payment_option...")
 
-            signal = True
-            if selected_payment_option == "quit" or "Quit" or 'Quit':
-                signal == False
+            elif submit_response == 'n' or 'N' or 'no' or 'NO':
+                print(signal, "sig wthin response of no")
+                signal = False
+                message = "select 'y' to complete order...Try again."
+                print(signal, "what's signal within submit no here...")
+                if signal == False:
+                    print(message)
+                        # signal = False
 
+            print(signal, "before slected payment_option choose pmt optoin or quit")
+            if signal == True:
+                selected_payment_option = input('Please choose a payment option. or Type "quit" to re-enter')
 
-
-            elif submit_response == 'n' or 'N':
-                print('Select "y" to complete order')
-                signal == False
-
+                if selected_payment_option == "quit" or "Quit" or 'Quit':
+                    signal == False
             else:
-                signal == True
+                signal == False
 
             if signal == True:
                 selected_payment_option_as_integer = int(selected_payment_option)
